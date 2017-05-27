aab31a7abde32abfb958b84612c2aff9dccb0c74
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-05-26 20:19:10 -1000

    completely works

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
-
+//img.style.display = none;
 
 var divElem = document.getElementsByClassName("name");
 
-//console.log(divElem);
+console.log(divElem);
 
 for(var i = 0; i<divElem.length; i++){
   
   divElem[i].addEventListener("click", toggle);
 
+}
+
+var pElem = document.getElementsByClassName("menu");
+
+for(var j = 0; j<pElem.length; j++){
+	pElem[j].style.display = "none";
 }
 
 function toggle(){
@@ -18,13 +24,13 @@ function toggle(){
 console.log(showMenu);
 
   if (showMenu.style.display==="none"){
-    console.log("if"+showMenu.style.display);
+    console.log("if "+showMenu.style.display);
     showMenu.style.display = "block";
     console.log("none to block:" + showMenu.style.display);
   }
 
   else{showMenu.style.display = "none";
-  console.log("else" + showMenu.style.display);
+  console.log("else: now " + showMenu.style.display);
        }
 
 	//console.log("HI");
